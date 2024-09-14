b1f8593dc22358a07721032f2da63d88297cd34f
Prabhashwaree <prabhashwaree57@gmail.com>
2024-09-14 10:52:34 +0530

    style :add setting page

--- src/app/components/post-login/admin-dashboard/system-settings/system-settings.component.ts
+++ src/app/components/post-login/admin-dashboard/system-settings/system-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-system-settings',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./system-settings.component.scss']
 })
 export class SystemSettingsComponent {
+  isEditing = false;
+  settingsForm: FormGroup;
+  passwordForm: FormGroup;
+  user = {
+    profilePicture: '',
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phone: '123456789',
+  };
 
+
+  constructor(private fb: FormBuilder) {
+    this.settingsForm = this.fb.group({
+      name: [this.user.name],
+      email: [this.user.email],
+      phone: [this.user.phone],
+      currentPassword: [''],
+    });
+
+
+    this.passwordForm = this.fb.group({
+      currentPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+
+  onEditClick() {
+    this.isEditing = true;
+  }
+
+
+  onSaveClick() {
+    if (this.settingsForm.valid) {
+      this.isEditing = false;
+      console.log('Form Data:', this.settingsForm.value);
+    }
+  }
+
+
+ 
+  onChangePasswordClick() {
+    if (this.passwordForm.valid) {
+      // Password change logic here
+    }
+  }
 }
+
+
+
+
+
